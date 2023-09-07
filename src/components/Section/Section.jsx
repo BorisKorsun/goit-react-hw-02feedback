8ba0e22ee5dbbdf0ef29a70b72b0f2ec182df7e0
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-07 18:41:10 +0200

    adds Notification component, adds prop-types

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+
 const Section = ({ title, children }) => {
   return (
     <section>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,15 @@
-const Statistics = () => {
-    return (
-      <>
-        <ul>
-          <li>Good:</li>
-          <li>Neutral:</li>
-          <li>Bad:</li>
-          <li>All feedbacks:</li>
-          <li>Percentage of positive feedbacks:%</li>
-        </ul>
-      </>
-    );
-  };
-  
-  export default Statistics;
-  
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>All feedbacks: {total}</li>
+        <li>Percentage of positive feedbacks: {positivePercentage}%</li>
+      </ul>
+    </>
+  );
+};
+
+export default Statistics;
